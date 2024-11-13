dee5d9b064b493c103d8582986d362063e748e2c
M-RB3 <rabea9220@gmail.com>
2024-11-13 23:01:01 +0400

    add endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,41 @@
-import React, { useState } from 'react';
-import './App.css';
-import ResultDisplay from './components/ResultDisplay';
+import React, { useState } from "react";
+import "./App.css";
+import ResultDisplay from "./components/ResultDisplay";
 function App() {
-  const [subreddit, setSubreddit] = useState('');
-  const [keyword, setKeyword] = useState('');
-  const [sentiment, setSentiment] = useState('');
-  const [confidence, setConfidence] = useState('');
+  const [subreddit, setSubreddit] = useState("");
+  const [keyword, setKeyword] = useState("");
+  const [result, setResult] = useState({});
 
-  const handleAnalyze = () => {
-  
-    setSentiment('Positive');
-    setConfidence('0.85');
+  const handleAnalyze = async () => {
+    const response = await fetch(`http://127.0.0.1:8000/analyze-sentiment`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ subreddit, keyword }),
+    });
+    const data = await response.json();
+    setResult(data);
   };
 
   return (
     <div className="app-container">
       <video autoPlay loop muted className="background-video">
-        <source src="https://videos.pexels.com/video-files/3248991/3248991-uhd_2560_1440_25fps.mp4" type="video/mp4" />
+        <source
+          src="https://videos.pexels.com/video-files/3248991/3248991-uhd_2560_1440_25fps.mp4"
+          type="video/mp4"
+        />
       </video>
 
       <div className="content">
         <h1>CrowdPulse: Reddit Sentiment Analysis</h1>
         <p>
-        Explore Reddit sentiment in real-time. Enter keywords or subreddit names to see insights on user opinions, analyzed through VADER sentiment scores, comment length, and upvote counts. Powered by machine learning, CrowdPulse provides accurate sentiment predictions and interactive visualizations. </p>
+          Explore Reddit sentiment in real-time. Enter keywords or subreddit
+          names to see insights on user opinions, analyzed through VADER
+          sentiment scores, comment length, and upvote counts. Powered by
+          machine learning, CrowdPulse provides accurate sentiment predictions
+          and interactive visualizations.{" "}
+        </p>
 
         <div className="input-container">
           <input
@@ -44,8 +57,6 @@ function App() {
         </button>
 
         <div className="result-display">
-
-
           {/* Display result and graphs */}
           <ResultDisplay sentiment={sentiment} confidence={confidence} />
         </div>
